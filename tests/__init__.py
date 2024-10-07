012eae143e14fbeeb031161134c9d69d7957f35d
Arash <arashmad.dev@gmail.com>
2024-10-07 18:19:26 +0200

    added missing setting

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,3 +4,11 @@ Initialization module for the tests package.
 This module is responsible for setting up the testing environment and making
 test-related functionality available to other parts of the project.
 """
+
+import os
+
+os.environ['service_namespace'] = 'test-service'
+
+os.environ['ROOT_PATH'] = ''
+os.environ['APP_PORT'] = "8000"
+os.environ['ALLOWED_CLIENT'] = "*"

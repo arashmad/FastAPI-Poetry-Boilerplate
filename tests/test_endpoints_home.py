5c8f605236a873a45267d85ec37ddb4c0bfc1da8
arashmad <arashmad.dev@gmail.com>
2024-12-24 22:03:05 +0100

    remove comments

--- tests/test_endpoints_home.py
+++ tests/test_endpoints_home.py
@@ -2,26 +2,6 @@
 
 """Tests for `data_provider` package."""
 
-# Data Provider,
-# A Python package for FastAPI providing the APIs to access geo-spatial datasets.
-#
-# Copyright (c) 2024, FernLab (GFZ Potsdam, fernlab@gfz-potsdam.de)
-#
-# This software was developed within the context [...]
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 from fastapi.testclient import TestClient
 
 from fastapi_poetry_boilerplate.core import setting
